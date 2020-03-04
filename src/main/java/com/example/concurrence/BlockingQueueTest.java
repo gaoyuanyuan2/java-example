766cdf00ff0c06fb87e9af9dcb134e1aa1fe4c6c
gaoyuanyuan2 <823789010@qq.com>
2020-03-04 15:22:08 +0800

    update

---
+++ src/main/java/com/example/concurrence/BlockingQueueTest.java
@@ -0,0 +1,24 @@
+package com.example.concurrence;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+public class BlockingQueueTest {
+
+    @Test
+    public void blockTest() throws InterruptedException {
+        offer(new ArrayBlockingQueue<>(2));
+        offer(new LinkedBlockingDeque<>(2));
+        offer(new PriorityBlockingQueue<>(2));
+        offer(new SynchronousQueue<>());
+    }
+
+    public void offer(BlockingQueue<Integer> queue) throws InterruptedException {
+        System.out.println("queue.offer(1) = " + queue.offer(1));
+        System.out.println("queue.offer(2) = " + queue.offer(2));
+        System.out.println("queue.offer(3) = " + queue.offer(3));
+        System.out.println("queue.size() = " + queue.size());
+        System.out.println("queue.take() = " + queue.take());
+    }
+}
--- src/main/java/com/example/concurrence/ThreadStateTest.java
+++ src/main/java/com/example/concurrence/ThreadStateTest.java
@@ -12,20 +12,20 @@ public class ThreadStateTest {
     @Test
     public void test(){
         List<Integer> list = Arrays.asList(1,2,3,4,5);
-
-        //java 9
-        Set<Integer> set = Set.of(1,2,3,4,5);
-
-        Map<Integer,String> map = Map.of(1,"A");
-
-        //以上实现都是不变对象，不过第一个除外
-
-        list = Collections.synchronizedList(list);
-        set = Collections.synchronizedSet(set);
-        map=Collections.synchronizedMap(map);
-
-        list = new CopyOnWriteArrayList<>(list);
-        set = new CopyOnWriteArraySet<>(set);
-        map = new ConcurrentHashMap<>(map);
+//
+//        //java 9
+//        Set<Integer> set = Set.of(1, 2, 3, 4, 5);
+//
+//        Map<Integer, String> map = Map.of(1, "A");
+//
+//        //以上实现都是不变对象，不过第一个除外
+//
+//        list = Collections.synchronizedList(list);
+//        set = Collections.synchronizedSet(set);
+//        map = Collections.synchronizedMap(map);
+//
+//        list = new CopyOnWriteArrayList<>(list);
+//        set = new CopyOnWriteArraySet<>(set);
+//        map = new ConcurrentHashMap<>(map);
     }
 }
